e21474f0b616b84a865e19b77bb980c0e18eeacf
Kyle McLean <kyledmclean@gmail.com>
2020-04-26 06:08:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,6 +49,7 @@ end
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
   # remember to return the updated hash
+  binding.pry
   holiday_supplies[season] << holiday_name
   supply_array.each do |supp|
     holiday_supplies[season][holiday_name] << supp
