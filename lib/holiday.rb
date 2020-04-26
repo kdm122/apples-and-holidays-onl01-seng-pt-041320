d715138526242064562d8b99b77e889146b202a4
Kyle McLean <kyledmclean@gmail.com>
2020-04-26 06:48:44 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -62,6 +62,16 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  winter_supplies = []
+  holiday_hash.each do |season, data|
+    if season == :winter
+      data.each do |holiday, data|
+        if holiday == :memorial_day
+        holiday_supplies[season][holiday] << supply
+        end
+      end
+    end
+  end
 
 end
 
